acfdbd57317d8b28b123d4155ba316729dbcc2c4
thailsondiniz <thailsondiniz94@gmail.com>
2024-08-14 11:35:47 -0300

    business

---
+++ android/app/src/main/kotlin/com/example/business_weellu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.business_weellu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/business_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BusinessHome extends StatefulWidget {
+  const BusinessHome({super.key});
+
+  @override
+  State<BusinessHome> createState() => _BusinessHomeState();
+}
+
+class _BusinessHomeState extends State<BusinessHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [],
+      ),
+    );
+  }
+}
